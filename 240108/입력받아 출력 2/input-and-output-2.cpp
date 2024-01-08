62f2512f112edd7f653475abfd978ea66ec5be31
kangyeon9525 <127291524+kangyeon9525@users.noreply.github.com>
2024-01-08 19:03:44 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240108/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int front, back;
+    cin >> front;
+    cin.get();
+    cin >> back;
+    cout << front << back;    
+    return 0;
+}
