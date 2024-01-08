a1bcd52a85f7a90fbad3e664b37a2a92f194cfff
kangyeon9525 <127291524+kangyeon9525@users.noreply.github.com>
2024-01-08 21:33:11 +0900

    Title: 배열 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240108/배열 채우기/filling-array.cpp
+++ 240108/배열 채우기/filling-array.cpp
@@ -4,10 +4,8 @@ using namespace std;
 int main() {
     int arr[10], cnt = 0;
     for (int i=0; i<10; i++) {
-        int tmp;
-        cin >> tmp;
-        if (tmp == 0) break;
-        arr[i] = tmp;
+        cin >> arr[i];
+        if (arr[i] == 0) break;
         cnt++;
     }
     for (int i=cnt-1; i>=0; i--) {
