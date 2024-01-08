f51b643c134819502b2132d00c021cea99437162
kangyeon9525 <127291524+kangyeon9525@users.noreply.github.com>
2024-01-08 19:13:05 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240108/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int x, y, z;
+    cin >> x; cin.get();
+    cin >> y; cin.get();
+    cin >> z;
+    cout << "010" << "-" << z << "-" << y;
+    return 0;
+}
