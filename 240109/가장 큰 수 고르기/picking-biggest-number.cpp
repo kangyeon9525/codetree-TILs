816862ab57ce0953dd227aabeae83209811fea02
kangyeon9525 <127291524+kangyeon9525@users.noreply.github.com>
2024-01-09 14:04:08 +0900

    Title: 가장 큰 수 고르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240109/가장 큰 수 고르기/picking-biggest-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+
+    for (int i=0; i<10; i++) {
+        cin >> arr[i];
+    }
+
+    int max_num = 0;
+    for (int i=0; i<10; i++) {
+        if (arr[i] > max_num) max_num = arr[i];
+    }
+    cout << max_num;
+    return 0;
+}
