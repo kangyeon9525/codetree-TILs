ac789eb8d6e67e6084dc6aed4d958a438f1338b1
kangyeon9525 <127291524+kangyeon9525@users.noreply.github.com>
2024-01-10 20:05:16 +0900

    Title: 트리의 부모 노드, Time: 8ms, Memory: 3MB, Status: Wrong Answer - CodeTree

---
+++ 240110/트리의 부모 노드/parent-node-of-the-tree.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#define MAX_N 100001
+
+using namespace std;
+
+vector<int> edges[MAX_N];
+vector<int> parent(MAX_N, 0);
+vector<bool> visited(MAX_N, false);
+
+void dfs(int n) {
+    visited[n] = true;
+    for (int i=0; i<edges[n].size(); i++) {
+        if (visited[n]) continue;
+        parent[i] = n;
+        dfs(i);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int n;
+    cin >> n;
+
+
+    for (int i=0; i<n; i++) {
+        int x, y;
+        cin >> x >> y;
+        edges[x].push_back(y);
+        edges[y].push_back(x);
+    }
+    dfs(1);
+
+    for (int i = 2; i <= n; ++i) {
+        cout << parent[i] << '\n';
+    }
+
+    
+    return 0;
+}
