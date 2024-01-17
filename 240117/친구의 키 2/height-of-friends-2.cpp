09a872bfc9588a023a5b1d40f6e4057c69c00292
kangyeon9525 <127291524+kangyeon9525@users.noreply.github.com>
2024-01-17 21:24:06 +0900

    Title: 친구의 키 2, Time: 7ms, Memory: 3MB, Status: Wrong Answer - CodeTree

---
+++ 240117/친구의 키 2/height-of-friends-2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <stack>
+
+using namespace std;
+
+const int MAX_N = 100000;
+
+vector<int> edges[MAX_N + 1];
+bool visited[MAX_N + 1];
+bool finished[MAX_N + 1];
+
+stack<int> reversed_order;
+
+void DFS(int x) {
+    for (int i = 0; i < (int)edges[x].size(); i++) {
+        int y = edges[x][i];
+
+        if (!visited[y]) {
+            visited[y] = true;
+            DFS(y);
+        }
+    }
+
+    reversed_order.push(x);
+    finished[x] = true;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    pair<int, int> friend_height[MAX_N];
+
+    for (int i = 0; i < m; i++) {
+        cin >> friend_height[i].first >> friend_height[i].second;
+        edges[friend_height[i].first].push_back(friend_height[i].second);
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            visited[i] = true;
+            DFS(i);
+        }
+    }
+
+    // 모순 검사
+    for (int i = 0; i < m; i++) {
+        int a = friend_height[i].first;
+        int b = friend_height[i].second;
+
+        if (!finished[a] || !finished[b]) {
+            cout << "Inconsistent";
+            return 0;
+        }
+    }
+
+    cout << "Consistent";
+
+    return 0;
+}
