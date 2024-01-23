dbfaa0be75da237be8549263c7d276c4c28a4eaf
kangyeon9525 <127291524+kangyeon9525@users.noreply.github.com>
2024-01-23 19:43:57 +0900

    Title: 가장 긴 좌우 대칭인 문자열 찾기 2, Time: 1047ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240123/가장 긴 좌우 대칭인 문자열 찾기 2/longest-palindrome-2.cpp
+++ 240123/가장 긴 좌우 대칭인 문자열 찾기 2/longest-palindrome-2.cpp
@@ -9,33 +9,21 @@ int LongestPalindromeLength(const string& str) {
     int n = str.size();
     int maxLength = 0;
 
-    // dp[i][j]는 문자열 str[i..j]가 좌우대칭인지 여부를 나타냄
-    vector<vector<bool>> dp(n, vector<bool>(n, false));
+    // 중심을 기준으로 확장하면서 좌우대칭인지 확인하는 함수
+    auto expandAroundCenter = [&](int left, int right) {
+        while (left >= 0 && right < n && str[left] == str[right]) {
+            int currentLength = right - left + 1;
+            maxLength = max(maxLength, currentLength);
+            left--;
+            right++;
+        }
+    };
 
-    // 길이가 1인 경우 모두 좌우대칭
     for (int i = 0; i < n; i++) {
-        dp[i][i] = true;
-        maxLength = 1;
-    }
-
-    // 길이가 2인 경우 좌우대칭일 때만 true
-    for (int i = 0; i < n - 1; i++) {
-        if (str[i] == str[i + 1]) {
-            dp[i][i + 1] = true;
-            maxLength = 2;
-        }
-    }
-
-    // 길이가 3 이상인 경우를 확인
-    for (int len = 3; len <= n; len++) {
-        for (int i = 0; i <= n - len; i++) {
-            int j = i + len - 1; // 현재 부분 문자열의 끝 인덱스
-            // 현재 부분 문자열의 양 끝이 같고 그 사이의 문자열도 좌우대칭일 때
-            if (str[i] == str[j] && dp[i + 1][j - 1]) {
-                dp[i][j] = true;
-                maxLength = len;
-            }
-        }
+        // 길이가 홀수인 경우
+        expandAroundCenter(i, i);
+        // 길이가 짝수인 경우
+        expandAroundCenter(i, i + 1);
     }
 
     return maxLength;
